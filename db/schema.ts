6035c5ad95773510a0fa38b933b4849b59591701
hdussert <hdussert@student.42.fr>
2025-10-12 00:20:58 +0200

    chore(db): setup database

---
+++ db/schema.ts
@@ -0,0 +1,11 @@
+import { InferSelectModel } from 'drizzle-orm'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const users = pgTable('users', {
+  id: text('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
+
+export type User = InferSelectModel<typeof users>
