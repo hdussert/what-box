6668fe1777b32c000501aeba782db447af94aabe
hdussert <hdussert@student.42.fr>
2025-12-27 18:15:53 +0100

    feat(db): add boxes_images table to store box image metadata

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'crypto'
 import { InferSelectModel, sql } from 'drizzle-orm'
-import { pgTable, text } from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
 // Common column definitions
 const id = () =>
@@ -9,13 +9,18 @@ const id = () =>
     .$default(() => randomUUID())
 
 const createdAt = () =>
-  text('created_at')
+  timestamp('created_at')
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull()
 
 const userIdRef = () =>
   text('user_id')
     .references(() => users.id, { onDelete: 'cascade' })
+    .notNull()
+
+const boxIdRef = () =>
+  text('box_id')
+    .references(() => boxes.id, { onDelete: 'cascade' })
     .notNull()
 
 // Tables definitions
@@ -34,5 +39,15 @@ export const boxes = pgTable('boxes', {
   createdAt: createdAt(),
 })
 
+export const boxesImages = pgTable('boxes_images', {
+  id: id(),
+  boxId: boxIdRef(),
+  userId: userIdRef(),
+  url: text('url').notNull(), // Public URL
+  pathname: text('pathname').notNull(), // Storage path
+  createdAt: createdAt(),
+})
+
 export type User = InferSelectModel<typeof users>
 export type Box = InferSelectModel<typeof boxes>
+export type BoxImage = InferSelectModel<typeof boxesImages>
