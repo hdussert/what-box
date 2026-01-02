6a30091d8e3eee8a0be522f046151c15fec000ac
hdussert <hdussert@student.42.fr>
2026-01-02 18:53:20 +0100

    refactor(ui): cleanup typography component

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -1,22 +1,28 @@
-type TypographyProps = {
-  children: React.ReactNode
-  className?: string
-}
+import { cn } from '@/lib/utils'
+import { ComponentProps } from 'react'
 
-const H1 = ({ children, className = '' }: TypographyProps) => (
-  <h1 className={`text-4xl font-extrabold ${className}`}>{children}</h1>
+const H1 = ({ className, ...props }: ComponentProps<'h1'>) => (
+  <h1 className={cn('text-4xl font-bold', className)} {...props}>
+    {props.children}
+  </h1>
 )
 
-const H2 = ({ children, className = '' }: TypographyProps) => (
-  <h2 className={`text-2xl font-bold ${className}`}>{children}</h2>
+const H2 = ({ className, ...props }: ComponentProps<'h2'>) => (
+  <h2 className={cn('text-2xl font-bold', className)} {...props}>
+    {props.children}
+  </h2>
 )
 
-const H3 = ({ children, className = '' }: TypographyProps) => (
-  <h3 className={`text-xl font-bold ${className}`}>{children}</h3>
+const H3 = ({ className, ...props }: ComponentProps<'h3'>) => (
+  <h3 className={cn('text-xl font-bold', className)} {...props}>
+    {props.children}
+  </h3>
 )
 
-const P = ({ children, className = '' }: TypographyProps) => (
-  <p className={`text-base ${className}`}>{children}</p>
+const P = ({ className, ...props }: ComponentProps<'p'>) => (
+  <p className={cn('text-base', className)} {...props}>
+    {props.children}
+  </p>
 )
 
 const Typography = { H1, H2, H3, P }
