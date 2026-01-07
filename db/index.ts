ba046c7e3c12c268c90776f4f02a9a4b3c4a1f4c
hdussert <hdussert@student.42.fr>
2026-01-07 16:52:34 +0100

    fix(db): use iso drizzle config for prod & dev

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,11 @@
 import { env } from '@/env'
 import { neon } from '@neondatabase/serverless'
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
-import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
+import { drizzle } from 'drizzle-orm/neon-http'
 
 import * as schema from './schema'
 
-export const db = env.VERCEL
-  ? drizzleNeon({
-      client: neon(env.DATABASE_URL),
-      schema,
-      casing: 'snake_case',
-    })
-  : drizzlePostgres(env.DATABASE_URL, { schema, casing: 'snake_case' })
+export const db = drizzle({
+  client: neon(env.DATABASE_URL),
+  schema,
+  casing: 'snake_case',
+})
