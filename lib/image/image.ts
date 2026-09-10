edf746aeffcafdd9ed9aa41655bb46772cd79df2
hdussert <hdussert@student.42.fr>
2026-09-10 10:52:26 +0200

    fix: delete items images when deleting boxes

--- lib/image/image.ts
+++ lib/image/image.ts
@@ -1,7 +1,7 @@
 import { db } from '@/db'
-import { ImageRecord, images } from '@/db/schema'
+import { ImageRecord, images, items } from '@/db/schema'
 import { getCurrentUser } from '@/lib/user'
-import { and, eq, inArray } from 'drizzle-orm'
+import { and, eq, inArray, or } from 'drizzle-orm'
 import 'server-only'
 
 /** Create a new Image record */
@@ -62,14 +62,24 @@ export async function getImagesByPathnames(
 
 export async function getBoxesImages(boxIds: string[]): Promise<ImageRecord[]> {
   const user = await getCurrentUser()
-
-  return db.query.images.findMany({
-    where: {
-      boxId: { in: boxIds },
-      userId: user.id,
-    },
-    orderBy: { createdAt: 'desc' },
-  })
+  return db
+    .select({
+      id: images.id,
+      userId: images.userId,
+      boxId: images.boxId,
+      itemId: images.itemId,
+      url: images.url,
+      createdAt: images.createdAt,
+      pathname: images.pathname,
+    })
+    .from(images)
+    .leftJoin(items, eq(images.itemId, items.id))
+    .where(
+      and(
+        eq(images.userId, user.id),
+        or(inArray(items.boxId, boxIds), inArray(images.boxId, boxIds)),
+      ),
+    )
 }
 
 export async function getItemsImages(
