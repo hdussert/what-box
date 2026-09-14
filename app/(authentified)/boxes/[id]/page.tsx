f33d8d5421ce136b976b3e6cc0c0a8432afaf09f
hdussert <hdussert@student.42.fr>
2026-09-14 21:49:15 +0200

    refactor: box/[id] Header

--- app/(authentified)/boxes/[id]/page.tsx
+++ app/(authentified)/boxes/[id]/page.tsx
@@ -1,23 +1,5 @@
-import Typography from '@/components/ui/typography'
-import { Box } from '@/db/schema'
+import BoxHeader from '@/app/components/box/BoxHeader'
 import { getBoxById } from '@/lib/box'
-
-// ==================== Header Component ====================
-
-type HeaderProps = {
-  box: Box
-}
-
-const Header = ({ box }: HeaderProps) => {
-  return (
-    <div className="flex flex-col gap-2 px-2">
-      <Typography.P className="uppercase font-bold text-muted-foreground text-sm font-mono">
-        Box {box.shortId}
-      </Typography.P>
-      <Typography.H2 className="uppercase">{box.name}</Typography.H2>
-    </div>
-  )
-}
 
 // ==================== Main Box Page Component ====================
 
@@ -29,7 +11,7 @@ const BoxPage = async ({ params }: BoxPageProps) => {
   const { id } = await params
   const box = await getBoxById(id)
 
-  return <Header box={box!} />
+  return <BoxHeader box={box!} />
 }
 
 export default BoxPage
---
+++ app/components/common/GoBackButton.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import ToolbarButton from '@/app/components/common/ToolbarButton'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+type GoBackButtonProps = {
+  className?: string
+}
+
+const GoBackButton = ({ className }: GoBackButtonProps) => {
+  const router = useRouter()
+  const goBack = () => {
+    router.back()
+  }
+  return (
+    <ToolbarButton onClick={goBack} size="icon">
+      <ArrowLeft size={48} />
+    </ToolbarButton>
+  )
+}
+
+export default GoBackButton
